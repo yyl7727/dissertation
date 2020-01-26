874ff87fb3880e89e45b1f77bda52920982f6cf2
yyl <772788236@qq.com>
2020-01-26 16:48:03 +0800

    指导教师表新增学生选择的论题id论题名称字段

--- src/main/java/com/gsm/dissertation/model/GuideTeacher.java
+++ src/main/java/com/gsm/dissertation/model/GuideTeacher.java
@@ -20,25 +20,35 @@ public class GuideTeacher {
      * 教师账号
      */
     @NotNull
-    private String t_account;
+    private String teacher_account;
 
     /**
      * 教师姓名
      */
     @NotNull
-    private String t_name;
+    private String teacher_name;
 
     /**
      * 学生账号
      */
     @NotNull
-    private String s_account;
+    private String student_account;
 
     /**
      * 学生姓名
      */
     @NotNull
-    private String s_name;
+    private String student_name;
+
+    /**
+     * 选择论题id
+     */
+    private int topic_id;
+
+    /**
+     * 选择论题名称
+     */
+    private String topic_name;
 
     /**
      * 论文选题状态
@@ -55,36 +65,52 @@ public class GuideTeacher {
         this.gid = gid;
     }
 
-    public String getT_account() {
-        return t_account;
+    public String getTeacher_account() {
+        return teacher_account;
     }
 
-    public void setT_account(String t_account) {
-        this.t_account = t_account;
+    public void setTeacher_account(String teacher_account) {
+        this.teacher_account = teacher_account;
     }
 
-    public String getT_name() {
-        return t_name;
+    public String getTeacher_name() {
+        return teacher_name;
     }
 
-    public void setT_name(String t_name) {
-        this.t_name = t_name;
+    public void setTeacher_name(String teacher_name) {
+        this.teacher_name = teacher_name;
     }
 
-    public String getS_account() {
-        return s_account;
+    public String getStudent_account() {
+        return student_account;
     }
 
-    public void setS_account(String s_account) {
-        this.s_account = s_account;
+    public void setStudent_account(String student_account) {
+        this.student_account = student_account;
     }
 
-    public String getS_name() {
-        return s_name;
+    public String getStudent_name() {
+        return student_name;
     }
 
-    public void setS_name(String s_name) {
-        this.s_name = s_name;
+    public void setStudent_name(String student_name) {
+        this.student_name = student_name;
+    }
+
+    public int getTopic_id() {
+        return topic_id;
+    }
+
+    public void setTopic_id(int topic_id) {
+        this.topic_id = topic_id;
+    }
+
+    public String getTopic_name() {
+        return topic_name;
+    }
+
+    public void setTopic_name(String topic_name) {
+        this.topic_name = topic_name;
     }
 
     public int getTopic_status() {

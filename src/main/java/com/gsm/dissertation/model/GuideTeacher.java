63e2458e9aaab179fbe65397e4dd0defe73b10b0
yyl <772788236@qq.com>
2020-01-27 11:56:08 +0800

    使用int时自动生成的表字段永远不可为空，修改为string

--- src/main/java/com/gsm/dissertation/model/GuideTeacher.java
+++ src/main/java/com/gsm/dissertation/model/GuideTeacher.java
@@ -1,5 +1,6 @@
 package com.gsm.dissertation.model;
 
+
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
@@ -43,7 +44,7 @@ public class GuideTeacher {
     /**
      * 选择论题id
      */
-    private int topic_id;
+    private String topic_id;
 
     /**
      * 选择论题名称
@@ -97,11 +98,11 @@ public class GuideTeacher {
         this.student_name = student_name;
     }
 
-    public int getTopic_id() {
+    public String getTopic_id() {
         return topic_id;
     }
 
-    public void setTopic_id(int topic_id) {
+    public void setTopic_id(String topic_id) {
         this.topic_id = topic_id;
     }
 
